24511c12d6b7dfcf8e6bf9d56d454961f38b4778
poonamk3 <poonamk@thoughtwin.com>
2023-08-11 12:01:07 +0530

    data

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookshop',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
+
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bookshop',
+#         'USER': 'postgres',
+#         'PASSWORD': 'psql',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
@@ -138,10 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 MEDIA_URL = '/media/'
 
-#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files/")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files/")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files/')
 
 CART_SESSION_ID = 'cart'
